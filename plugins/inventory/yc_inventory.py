e93fb2ae1a8ca49a4c7a4e773a22ad32c7110a63
Sergey Shadurskiy <s.shadurskiy@gmail.com>
2022-04-05 15:33:47 +0500

    yandex cloud inventory plugin implemented

---
+++ plugins/inventory/yc_inventory.py
@@ -0,0 +1,82 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+    name: yc_inventory
+    plugin_type: inventory
+    short_description: Returns Ansible inventory from Yandex Cloud
+    description: Returns Ansible inventory from Yandex Cloud
+    options:
+      plugin:
+          description: Name of the plugin
+          required: true
+          choices: ['my_csv_plugin']
+'''
+
+from ansible.plugins.inventory import BaseInventoryPlugin
+from ansible.errors import AnsibleError, AnsibleParserError
+import json
+import os 
+import subprocess
+
+
+class InventoryModule(BaseInventoryPlugin):
+    NAME = 'netology86.yandex_cloud_elk.yc_inventory'
+
+    def get_instance_info(self):
+
+        # yc compute instance list --format=json
+        
+        vars = [
+            "yc", "compute", "instance", "list", "--format=json"
+        ]
+        process = subprocess.Popen(vars, 
+                        stdout=subprocess.PIPE,
+                        universal_newlines=True)
+        process.wait()
+        data, err = process.communicate()
+        if process.returncode == 0:
+            return data
+        else:
+            print("Error:", err)
+        return "{}"
+
+
+    def verify_file(self, path):
+        '''Return true/false if this is possibly a valid file for this plugin toconsume'''
+        valid = False
+        if super(InventoryModule, self).verify_file(path):
+            # base class verifies that file exists and is readable by current
+            # user
+            if path.endswith(('yc.yaml','yc.yml','yc_inventory.yaml','yc_inventory.yml')):
+                valid = True
+        return valid
+
+
+    def parse(self, inventory, loader, path, cache):
+        '''Return dynamic inventory from source '''
+        super(InventoryModule, self).parse(inventory, loader, path, cache)
+        # Read the inventory YAML file
+    #    self._read_config_data(path)
+    #    try:
+    #        # Store the options from the YAML file
+    #        self.plugin = self.get_option('plugin')
+    #        self.inv_dir = self.get_option('path_to_inventory')
+    #        self.inv_file = self.get_option('csv_file')
+    #    except Exception as e:
+    #        raise AnsibleParserError(
+    #            'All correct options required: {}'.format(e))
+    #    # Call our internal helper to populate the dynamic inventory
+    #    self._populate()
+
+        instance_info = json.loads(self.get_instance_info())
+
+        self.inventory.add_group("yacloud")
+
+        for instance in instance_info:
+            # print(instance_info['network_interfaces'][0]['primary_v4_address']['one_to_one_nat']['address'])
+            instance['ipv4'] = instance['network_interfaces'][0]['primary_v4_address']['one_to_one_nat']['address']
+            if not "name" in instance:
+                instance['name'] = instance['id']
+            self.inventory.add_host(instance["name"], group="yacloud")
+            self.inventory.set_variable(instance["name"], 'ansible_host', instance['ipv4'])
